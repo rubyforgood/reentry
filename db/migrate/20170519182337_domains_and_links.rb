34ecf1d1bed973a984524c70dfeae5929e3ef6b5
JC Avena <jcavena@gmail.com>
2017-05-19 14:56:00 -0400

    Add a domains table and model.

---
+++ db/migrate/20170519182337_domains_and_links.rb
@@ -0,0 +1,17 @@
+class Domains < ActiveRecord::Migration[5.1]
+  def change
+    create_table :domains do |t|
+      t.string :name
+      t.string :kind
+      t.text :url
+      t.text :description
+      t.integer :search_depth
+      t.string :status, :default => 'active', :null => false
+      t.timestamp :status_date
+      t.timestamp :searched_at
+      t.timestamps
+    end
+
+
+  end
+end
