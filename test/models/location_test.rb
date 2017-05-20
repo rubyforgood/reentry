d40d9092f36d402e247b69df04078b27607b3756
Ben Miller <bjmllr@gmail.com>
2017-05-20 16:14:02 -0400

    add tests for domain_services & location_services
    needed a migration to re-type the reference columns; since we are pre-production
    we don't make any attempt to cast strings to integers and instead drop and
    recreate those tables

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -4,4 +4,19 @@ describe Location do
   it 'must be valid' do
     assert_equal true, Location.new(domain: Domain.new).valid?
   end
+
+  describe '#services' do
+    it 'returns an array' do
+      skip "services is both a columns and an association currently"
+      assert_equal [], Location.create.services.to_a
+    end
+
+    it 'returns a collection of Service' do
+      skip "services is both a column and an association currently"
+      service = Service.create
+      location = Location.create(domain: Domain.new)
+      location.services << service
+      assert location.services.include?(service)
+    end
+  end
 end
