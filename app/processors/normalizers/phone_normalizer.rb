0d36bf693230a24334008fd4abca6cf94e31bcde
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2017-05-20 12:05:04 -0400

    WIP

--- app/processors/normalizers/phone_normalizer.rb
+++ app/processors/normalizers/phone_normalizer.rb
@@ -1,39 +1,41 @@
 class PhoneNormalizer
   PHONE_FORMATS = {
-    'X-XXX-XXX-XXXX' => /\d{1}-\d{3}-\d{3}-\d{4}/,
-    'XXX-XXX-XXXX'   => /\d{3}-\d{3}-\d{4}/,
-    '(XXX)XXX-XXXX'  => /\(.*?\d{3}\)\d{3}-\d{4}/,
-    'XXX.XXX.XXXX'   => /\d{3}\.\d{3}\.\d{4}/,
-    'XXXXXXXXXX'     => /\d{10}/,
+    'XXXXXXXXXX' => /\d{10}/,
   }.freeze
 
-  class << self
-    def normalize(number_string)
-      partials     = number_string.split(' x')
-      phone_number = PhoneNumber.new(partials)
+  # "(410) 837-^M1800 ext.^M240 / (410)^M887-0295"
 
-      if phone_number.has_extension?
-        reformatted_string_with_extension phone_number
-      else
-        reformat_string phone_number.number
+  class << self
+    def normalize(phone_data_string)
+      numbers_array = phone_data_string.split('/')
+
+      numbers_array.map do |num|
+        partials     = num.split(' x')
+        phone_number = format_phone_number(PhoneNumber.new(nil, partials))
+
+        PhoneNumber.new(phone_number, partials)
       end
     end
 
     private
 
-    def reformatted_string_with_extension(phone_number)
-      "#{reformat_string(phone_number.number)} ext: #{phone_number.extension}"
+    def format_phone_number(phone_number)
+      if phone_number.has_extension?
+        reformat_string_with_extension(phone_number)
+      else
+        reformat_string(phone_number.partials.first)
+      end
+    end
+
+    def reformat_string_with_extension(phone_number)
+      "#{reformat_string(phone_number.partials.first)} ext: #{phone_number.extension}"
     end
 
     def reformat_string(phone_number, formatted_as: PHONE_FORMATS)
       stripped_number = phone_number.gsub(/\D/, '')
 
       case stripped_number
-      when formatted_as['X-XXX-XXX-XXXX'] then stripped_number
-      when formatted_as['XXX-XXX-XXXX']   then stripped_number
-      when formatted_as['(XXX)XXX-XXXX']  then stripped_number.tr('(', '').tr(')', '-')
-      when formatted_as['XXX.XXX.XXXX']   then stripped_number.tr('.',  '-')
-      when formatted_as['XXXXXXXXXX']     then hyphenate(stripped_number)
+      when formatted_as['XXXXXXXXXX'] then hyphenate(stripped_number)
       else
         fail_phone_format(phone_number)
         phone_number
@@ -54,11 +56,7 @@ class PhoneNormalizer
     end
   end
 
-  PhoneNumber = Struct.new(:partials) do
-    def number
-      partials.first
-    end
-
+  PhoneNumber = Struct.new(:number, :partials) do
     def extension
       partials.try(:[], 1)
     end
--- test/processors/normalizers/phone_normalizer_test.rb
+++ test/processors/normalizers/phone_normalizer_test.rb
@@ -4,65 +4,72 @@ describe PhoneNormalizer do
   let(:normalizer) { PhoneNormalizer }
 
   describe '.normalize' do
-    describe 'with a string in the (XXX) XXX-^MXXX format' do
+    describe 'with a string containing two numbers' do
       it 'returns a string in the XXX-XXX-XXXX format' do
-        number = '(234) 567-^M8910'
-        normalized_number = normalizer.normalize number
+        #### WIP
+      end
+    end
 
-        assert_equal '234-567-8910', normalized_number
+    describe 'with a string in the (XXX) XXX-^MXXX format' do
+      it 'returns an array containing the normalized PhoneNumber' do
+        number       = '(234) 567-^M8910'
+        result       = normalizer.normalize number
+        phone_number = result.first
+
+        assert_instance_of Array, result
+        assert_equal '234-567-8910', phone_number.number
       end
     end
 
     describe 'with a string in the X-XXX-XXX-XXXX format' do
-      it 'returns a string in the X-XXX-XXX-XXXX format' do
-        number            = '1-234-567-8910'
-        normalized_number = normalizer.normalize number
+      it 'returns an array containing the normalized PhoneNumber' do
+        number = '1-234-567-8910'
+        result = normalizer.normalize(number).first
 
-        assert_equal '1-234-567-8910', normalized_number
+        assert_equal '1-234-567-8910', result.number
       end
     end
 
     describe 'with a string in the XXX-XXX-XXXX format' do
-      it 'returns a string in the XXX-XXX-XXXX format' do
-        number            = '234-567-8910'
-        normalized_number = normalizer.normalize number
+      it 'returns an array containing the normalized PhoneNumber' do
+        number = '234-567-8910'
+        result = normalizer.normalize(number).first
 
-        assert_equal '234-567-8910', normalized_number
+        assert_equal '234-567-8910', result.number
       end
     end
 
-
     describe 'with a string in the (XXX)XXX-XXXX format' do
-      it 'returns a string in the XXX-XXX-XXXX format' do
-        number            = '(234)567-8910'
-        normalized_number = normalizer.normalize number
+      it 'returns an array containing the normalized PhoneNumber' do
+        number = '(234)567-8910'
+        result = normalizer.normalize(number).first
 
-        assert_equal '234-567-8910', normalized_number
+        assert_equal '234-567-8910', result.number
       end
     end
 
     describe 'with a string in the XXX.XXX.XXXX format' do
-      it 'returns a string in the XXX-XXX-XXXX format' do
-        number            = '234.567.8910'
-        normalized_number = normalizer.normalize number
+      it 'returns an array containing the normalized PhoneNumber' do
+        number = '234.567.8910'
+        result = normalizer.normalize(number).first
 
-        assert_equal '234-567-8910', normalized_number
+        assert_equal '234-567-8910', result.number
       end
     end
 
     describe 'with an extension' do
-      it 'returns a string in the correct format with an extension' do
+      it 'returns an array containing the normalized PhoneNumber with an extension' do
         number_1            = '234.567.8910 x91233'
         number_2            = '(234)567-8910 x91233'
         number_3            = '1-234-567-8910 x91233'
 
-        normalized_number_1 = normalizer.normalize number_1
-        normalized_number_2 = normalizer.normalize number_2
-        normalized_number_3 = normalizer.normalize number_3
+        normalized_number_1 = normalizer.normalize(number_1).first
+        normalized_number_2 = normalizer.normalize(number_2).first
+        normalized_number_3 = normalizer.normalize(number_3).first
 
-        assert_equal '234-567-8910 ext: 91233', normalized_number_1
-        assert_equal '234-567-8910 ext: 91233', normalized_number_2
-        assert_equal '1-234-567-8910 ext: 91233', normalized_number_3
+        assert_equal '234-567-8910 ext: 91233', normalized_number_1.number
+        assert_equal '234-567-8910 ext: 91233', normalized_number_2.number
+        assert_equal '1-234-567-8910 ext: 91233', normalized_number_3.number
       end
     end
 
@@ -78,11 +85,11 @@ describe PhoneNormalizer do
         normalizer.normalize invalid_number_format
       end
 
-      it 'returns the original input' do
+      it 'returns the original input ' do
         invalid_number_format = 'Call 211'
         unnormalized_number   = normalizer.normalize invalid_number_format
 
-        assert_equal invalid_number_format, unnormalized_number
+        assert_equal invalid_number_format, unnormalized_number.first.number
       end
     end
   end
