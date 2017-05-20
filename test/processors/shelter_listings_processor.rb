c22d646111078526fa3c2bd4bb3350625170681c
dushyanthi pieris <dushi27@gmail.com>
2017-05-20 14:42:12 -0400

    added the shelter_listings_processor test stucture

--- test/processors/shelter_listings_processor.rb
+++ test/processors/shelter_listings_processor.rb
@@ -6,31 +6,33 @@ class ShelterListingsProcessorTest < ActiveSupport::TestCase
       url: 'http://www.shelterlistings.org/state/maryland.html'
     )
   end
-
   let(:details_page_url) {'http://www.shelterlistings.org/details/31018/'}
   
+  describe '.perform' do
+    describe '.run_shelter_listings_spider' do
+      it '.get_list_of_cities returns an ary of city tags' do 
+      end 
 
-  let(:shelter_listings_MD_page) do 
-    VCR.use_cassette("shelter_listings_MD_html") do 
-      assert PerformSpider.get_website_html(url: state_domain.url)
+      it '.get_list_of_detail_links returns an ary of detail links' do 
+      end 
+
+      it '.extract_data_from_details_page returns a data hash' do
+      end
+
+      it '.extract_data_from_details_page returns a data hash' do
+      end
+
+      describe '.store_location' do 
+        it '.extract_shelterlistings_address extracts an address' do 
+        end
+
+        it '.store_location create a location record' do 
+        end 
+      end
+
     end
+
   end
 
-  let(:shelter_listings_detail_page) do 
-    VCR.use_cassette("shelter_listings_details_page_html") do 
-      assert PerformSpider.get_website_html(url: details_page_url)
-    end
-  end
 
-  describe '.get_list_of_cities' do 
-    links_ary = 
-    get_list_of_detail_links(links_array: links_ary)
-  #   cities_list = get_list_of_cities(url: domain_url)
-  #   details_links = get_list_of_detail_links(links_array: cities_list)
-  #   details_links.each do |link| 
-  #     data_hash = extract_data_from_details_page(details_url:link)
-  #     loc = store_location(location_data_hash: data_hash)
-  #     Rails.logger.info "stored location" if loc
-  #   end
-  end
 end
