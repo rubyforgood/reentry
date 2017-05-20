809aca7bc96f572fbd22c47cb37dd86a9b261e33
Kenneth Ng <ngkenneth@gmail.com>
2017-05-19 17:09:30 -0700

    added function to extract address form ShelterListings site rubyforgood/reentry#8

--- app/processors/shelter_listings_spider.rb
+++ app/processors/shelter_listings_spider.rb
@@ -2,54 +2,64 @@ require 'open-uri'
 require 'nokogiri'
 
 class ShelterListingsSpider
-	doc = Nokogiri::HTML(open('http://www.shelterlistings.org/state/maryland.html'))
 
-	# collect city links
-	link_tags = doc.css('.wrapper #content .post.page-content table a')
-	city_links = []
-	detail_urls = []
+	def run
+		doc = Nokogiri::HTML(open('http://www.shelterlistings.org/state/maryland.html'))
 
-	link_tags.each do |tag|
-		city_links << tag['href'] 
-		# limit the number of calls made 
-		# so we don't make them too angry :)
-		break if city_links.count > 1  
-	end
-	puts city_links.count
+		# collect city links
+		link_tags = doc.css('.wrapper #content .post.page-content table a')
+		city_links = []
+		detail_urls = []
 
-	# collect location urls
-	city_links.each do |link|
-		doc = Nokogiri::HTML(open(link))
-		detail_urls << doc.css('table tr td a')[0]['href']
-		p "collected #{detail_urls.count} detail links"
+		link_tags.each do |tag|
+			city_links << tag['href'] 
+			# limit the number of calls made 
+			# so we don't make them too angry :)
+			break if city_links.count > 1  
+		end
+		puts city_links.count
 
-		# limit the number of calls made 
-		# so we don't make them too angry :)
-		break if detail_urls.count > 10
-	end
+		# collect location urls
+		city_links.each do |link|
+			doc = Nokogiri::HTML(open(link))
+			detail_urls << doc.css('table tr td a')[0]['href']
+			p "collected #{detail_urls.count} detail links"
 
-	# loop through each details page and collect
-	# organization name, phone, and address
+			# limit the number of calls made 
+			# so we don't make them too angry :)
+			break if detail_urls.count > 10
+		end
 
-	detail_urls.each do |link|
-		doc = Nokogiri::HTML(open(link))
-		name = doc.css('.post.page-content h2').children.first.text
-		address = doc.css('.post.page-content br')[4].next.text.strip
-		phone = doc.css('.post.page-content br')[5].next.text.strip
-		raw_website = doc.css('.post.page-content .entry-content #grey_bar')[2].children[0].text
-		# raw_website.nil? &&  ? (website = 'N/A') : (website = raw_website.gsub("Website: ", ""))
-		website = link
-		service = 'Homeless Shelter'
+		# loop through each details page and collect
+		# organization name, phone, and address
+
+		detail_urls.each do |link|
+			doc = Nokogiri::HTML(open(link))
+			name = doc.css('.post.page-content h2').children.first.text
+			address = extract_shelterlistings_address(doc)
+			phone = doc.css('.post.page-content br')[5].next.text.strip
+			raw_website = doc.css('.post.page-content .entry-content #grey_bar')[2].children[0].text
+			# raw_website.nil? &&  ? (website = 'N/A') : (website = raw_website.gsub("Website: ", ""))
+			website = link
+			service = 'Homeless Shelter'
 
 
-		puts "#{name}|#{address}|#{phone}|#{service}|#{website}"
+			puts "#{name}|#{address}|#{phone}|#{service}|#{website}"
+		end
 	end
 
 	private
 
-		def extract_data(url: raise, css_selectors: )
-			doc = Nokogiri::HTML(open(url))
-			doc.css(css_selectors)
-		end
+	def extract_data(url: raise, css_selectors: )
+		doc = Nokogiri::HTML(open(url))
+		doc.css(css_selectors)
+	end
 
+	# Get the lat lng from the HTML inside its js, and return the street address
+	def extract_shelterlistings_address(doc_html )
+		google_map_js = doc_html.css("script")[9].text
+		lat, lng = google_map_js.scan(/-?\d+\.\d+/); 
+		geo_localization = "#{lat},#{lng}"
+		query = Geocoder.search(geo_localization).first
+	end
 end
--- test/processors/shelter_listings_spider_test.rb
+++ test/processors/shelter_listings_spider_test.rb
@@ -1,5 +1,46 @@
 require 'test_helper'
+require 'open-uri'
+require 'nokogiri'
 
-class ShelterListingsSpider < ActiveSupport::TestCase
-	
+class ShelterListingsSpiderTest < ActiveSupport::TestCase
+  myUrl = 'http://www.shelterlistings.org/details/38685/'
+  doc = Nokogiri::HTML(open(myUrl))
+
+  test "Run extract_shelterlistings_address within shelter_listings_spider" do
+    slist = ShelterListingsSpider.new
+    assert slist.run
+  end
+
+  test "Return Maryland address from extract_shelterlistings_address method" do
+    expected = "3104 Pelham Avenue Belair - Edison Baltimore Maryland United States 21213 1745"
+
+    slist = ShelterListingsSpider.new
+    address = slist.send :extract_shelterlistings_address, doc
+    outcome = address.data["address_components"].map do |hash|
+      hash["long_name"]
+    end.join(" ")
+
+    assert_equal outcome, expected
+    #binding.pry
+
+  end
 end
+
+
+ # @data=
+ #  {"address_components"=>
+ #    [{"long_name"=>"3104", "short_name"=>"3104", "types"=>["street_number"]},
+ #     {"long_name"=>"Pelham Avenue", "short_name"=>"Pelham Ave", "types"=>["route"]},
+ #     {"long_name"=>"Belair - Edison", "short_name"=>"Belair - Edison", "types"=>["neighborhood", "political"]},
+ #     {"long_name"=>"Baltimore", "short_name"=>"Baltimore", "types"=>["locality", "political"]},
+ #     {"long_name"=>"Maryland", "short_name"=>"MD", "types"=>["administrative_area_level_1", "political"]},
+ #     {"long_name"=>"United States", "short_name"=>"US", "types"=>["country", "political"]},
+ #     {"long_name"=>"21213", "short_name"=>"21213", "types"=>["postal_code"]},
+ #     {"long_name"=>"1745", "short_name"=>"1745", "types"=>["postal_code_suffix"]}],
+ #   "formatted_address"=>"3104 Pelham Ave, Baltimore, MD 21213, USA",
+ #   "geometry"=>
+ #    {"location"=>{"lat"=>39.323683, "lng"=>-76.569064},
+ #     "location_type"=>"ROOFTOP",
+ #     "viewport"=>
+ #      {"northeast"=>{"lat"=>39.3250319802915, "lng"=>-76.56771501970849},
+ #       "southwest"=>{"lat"=>39.3223340197085, "lng"=>-76.57041298029151}}},
