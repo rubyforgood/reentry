0e3e227e4030f9a0a92a341fbfd85eddf30a3689
dushyanthi pieris <dushi27@gmail.com>
2017-05-20 14:04:53 -0400

    added perform spider tests

--- test/processors/perform_spider_test.rb
+++ test/processors/perform_spider_test.rb
@@ -9,7 +9,6 @@ class PerformSpiderTest < ActiveSupport::TestCase
 
   let(:details_page_url) {'http://www.shelterlistings.org/details/31018/'}
   
-
   describe '.get_website_html' do 
     it 'get maryland page' do
         VCR.use_cassette("shelter_listings_MD_html") do 
@@ -20,6 +19,15 @@ class PerformSpiderTest < ActiveSupport::TestCase
     it 'get details page' do
         VCR.use_cassette("shelter_listings_details_html") do 
           assert PerformSpider.new.get_website_html(url: details_page_url)
+        end
+    end
+  end
+
+  describe '.extract_data' do 
+    it 'runs' do
+        VCR.use_cassette("shelter_listings_MD_html") do 
+          maryland_page = PerformSpider.new.get_website_html(url: domain.url)
+          assert PerformSpider.new.extract_data(html: maryland_page, css_selectors:  '.wrapper #content .post.page-content table a')
         end
     end
   end
