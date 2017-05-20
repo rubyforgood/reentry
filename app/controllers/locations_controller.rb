aa65cc3a415a0806dd8faab43c20c122ce2b92a2
Melissa Miller <lis.d.miller@gmail.com>
2017-05-20 11:47:06 -0400

    adds edit action

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,4 +2,28 @@ class LocationsController < ApplicationController
   def index
     @locations = Location.all
   end
+
+  def show
+    @location = Bolation.find(params[:id])
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+
+    if @location.update_attributes(location_params)
+      redirect_to action: 'index', notice: "Location updated"
+    else
+      render action: 'edit'
+    end
+  end
+
+  private
+
+  def location_params
+    params.require(:location).permit(:county, :name, :address, :phone, :website, :services, :type_of_services)
+  end
 end
