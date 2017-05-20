2d2d3a18d1ea4c2717cd79cbefe44c84bb2ffb40
JC Avena <jcavena@gmail.com>
2017-05-20 15:04:50 -0400

    Add linking tables for domain - services and location - services.

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,6 @@
 class Service < ApplicationRecord
+  has_many :domain_services
+  has_many :domains, through: :domain_services
+  has_many :location_services
+  has_many :locations, through: :location_services
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20170520182502) do
     t.index ["link_id"], name: "index_documents_on_link_id"
   end
 
+  create_table "domain_services", force: :cascade do |t|
+    t.string "domain_id"
+    t.string "service_id"
+    t.boolean "active", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "domains", force: :cascade do |t|
     t.string "name"
     t.string "kind"
@@ -41,6 +49,14 @@ ActiveRecord::Schema.define(version: 20170520182502) do
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.datetime "retrieved_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "location_services", force: :cascade do |t|
+    t.string "location_id"
+    t.string "service_id"
+    t.boolean "active", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
