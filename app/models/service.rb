5b2b33fff176f76d36959935c9b326f72ed80f99
JC Avena <jcavena@gmail.com>
2017-05-21 10:23:56 -0400

    Ensure only location_services is validating for status.

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,6 +4,4 @@ class Service < ApplicationRecord
   has_many :location_services
   has_many :locations, through: :location_services
 
-  validates :status, inclusion: { in: %w(active defunct inactive),
-    message: "%{value} is not a valid status" }
 end
