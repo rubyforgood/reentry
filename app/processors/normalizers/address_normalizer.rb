986f2ff3b0c6bc92c778ad0a642d5f64488d8d3a
dushyanthi pieris <dushi27@gmail.com>
2017-05-21 12:10:23 -0400

    update address normalizer

--- app/processors/normalizers/address_normalizer.rb
+++ app/processors/normalizers/address_normalizer.rb
@@ -1,5 +1,19 @@
 class AddressNormalizer
+
 	def self.normalize(address_string: raise)
-		return {}
+		query = Geocoder.search(address_string).first
+		address1,city,state_zip,country = query.data["formatted_address"].split(',')
+	    state,zip = state_zip.split(' ')
+	    output = { 
+		  'address' => query.data["formatted_address"],
+	      'address1' => address1,
+	      'address2' => nil,
+		    'city' => city,
+		    'state_province' => state,
+		    'postal_code' => zip,
+		    'country' => country,
+		    'lat' => query.data["geometry"]["location"]["lat"],
+		    'lng' => query.data["geometry"]["location"]["lng"]  
+	    }
 	end
 end
