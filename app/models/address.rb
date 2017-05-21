9b1846d60f52addb2ece6ba50ba82f24841faeca
dushyanthi pieris <dushi27@gmail.com>
2017-05-21 11:52:14 -0400

    added address validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,16 @@
 class Address < ApplicationRecord
-	# validates :location_id, presence: true
-	# validates :address_1, presence: true
-	# validates :city, presence: true
-	# validates :state_provice, presence: true
-	# validates :postal_code, presence: true
-	# validates :country, presence: true
+	validates :location_id, presence: true
+	validates :address_1, presence: true
+	validates :city, presence: true
+	validates :state_provice, presence: true
+	validates :postal_code, presence: true
+	validates_length_of :copostal_codeuntry, :maximum => 5
+	validates_length_of :postal_code, :minimum => 5
+	validates :country, presence: true
+	validates_length_of :country, :maximum => 2
+	validates_length_of :country, :minimum => 2
+
+	def validate_postal_code
+		regex = /\A\d{5}(-\d{4})?\z/
+	end
 end
