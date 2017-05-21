1ee79aea96a33fc6092709c7f8d0fb8545e8ce28
JC Avena <jcavena@gmail.com>
2017-05-21 10:23:01 -0400

    Ensure only valid statuses are added to location_services.

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,4 +3,7 @@ class Service < ApplicationRecord
   has_many :domains, through: :domain_services
   has_many :location_services
   has_many :locations, through: :location_services
+
+  validates :status, inclusion: { in: %w(active defunct inactive),
+    message: "%{value} is not a valid status" }
 end
