c28e94fd6eb62af745794fd16f759b521d527d8a
Craig Zaccaro <czaccaro@live.com>
2017-06-09 10:20:00 -0400

    Check signup and reset mailers locally. Begin crud for user manager. In progress.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  resources :users as :manage_users
+  devise_for :users, :path_prefix => 'app'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#index'
