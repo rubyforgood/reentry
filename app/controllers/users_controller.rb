b07d99abf031853a093745710dd7de297a551556
Craig Zaccaro <czaccaro@live.com>
2017-06-11 22:39:49 -0400

    User crud for admins. Admin name to devise registration.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,30 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
-  def manage
+
+  def index
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+  if @user.update_attributes(user_params)
+    redirect_to user_path
+    flash[:success] = "User updated"
+  else
+    redirect_to edit_user_path
+    flash[:danger] = "Unable to update user"
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+    flash[:success] = "User removed"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit()
   end
 end
